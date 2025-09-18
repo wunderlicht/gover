5fade2a12a053891a7faf61b45f212e642a2db67
wunderlicht <wunderlich@actualchange.eu>
2025-09-18 11:38:00 +0200

    change os.Exit() to panic() to gracefully handle defers

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"regexp"
 )
 
@@ -18,23 +17,19 @@ func main() {
 
 	resp, err := http.Get(site.url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't get url. %v\n", err)
-		os.Exit(1)
+		panic("Can't get url. " + err.Error())
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stderr, "Got other status than 200 OK. %v\n", resp.StatusCode)
-		os.Exit(1)
+		panic("Got other status than 200 OK: " + resp.Status)
 	}
 	bodyByte, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't read response Body. %v\n", err)
-		os.Exit(1)
+		panic("Can't read response Body. " + err.Error())
 	}
 
 	re := regexp.MustCompile(site.regexp)
 	version := re.FindStringSubmatch(string(bodyByte))
 	fmt.Printf("Version on go.dev: %s\n", version[1])
-
 }
